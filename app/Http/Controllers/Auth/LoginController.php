eaca15a64c72c23e720fc02023927c2532b72adf
Hori Taichi <thori0802@gmail.com>
2018-09-08 13:18:46 +0900

    ログイン画面実装

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -21,11 +24,11 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login.
+     * 認証成功した時に遷移するリダイレクト先
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -35,5 +38,46 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * 認証する際、loginIdフィールドを認証に利用
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'login_id';
+    }
+
+    /**
+     * ログイン
+     * @return string
+     */
+    public function login()
+    {
+        $login_id = request('login_id');
+        $password = request('password');
+
+        // TODO: 全ロール対応の管理画面にする
+        if (Auth::attempt(['login_id' => $login_id, 'password' => $password, 'role' => 50])) {
+            return redirect()->intended('/');
+        } else {
+            throw ValidationException::withMessages([
+                $this->username() => [trans('auth.failed')],
+            ]);
+        }
+    }
+
+    /**
+     * ログアウト
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect()->intended('login');
+
     }
 }
--- app/User.php
+++ app/User.php
@@ -11,20 +11,78 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
+     * ロール
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    const ROLE_VIEW_ONLY = 10;
+    const ROLE_ADVERTISER = 20;
+    const ROLE_AGENCY = 30;
+    const ROLE_MEDIA = 40;
+    const ROLE_ADMIN = 50;
 
     /**
-     * The attributes that should be hidden for arrays.
-     *
+     * ステータス
+     */
+    const STATUS_ACTIVE = 1;
+    const STATUS_INACTIVE = 2;
+    const STATUS_DELETE = 99;
+
+    /**
+     * accountsテーブルに変更
+     * @var string
+     */
+    protected $table = 'accounts';
+
+
+    /**
+     * プライマリキー
+     * @var string
+     */
+    protected $primaryKey = 'account_id';
+
+
+    /**
+     * 参照禁止項目
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+      'password_hash'
     ];
+
+
+    /**
+     * ユーザーの主キーを取得
+     * @return string
+     */
+    public function getAuthIdentifierName()
+    {
+        return 'account_id';
+    }
+
+    /**
+     * 認証時のパスワード（ハッシュ化済）を取得
+     * @return mixed|string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password_hash;
+    }
+
+    /**
+     * ※RemenberTokenは使用しない
+     * @param string $value
+     * @return bool|void
+     */
+    public function setRememberToken($value)
+    {
+        return true;
+    }
+
+    /**
+     * ※RemenberTokenは使用しない
+     * @return null|string
+     */
+    public function getRememberToken()
+    {
+        return null;
+    }
 }
