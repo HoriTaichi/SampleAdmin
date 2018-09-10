5b566d2e025aa3e77bae020f27806668e1ef767d
Hori Taichi <thori0802@gmail.com>
2018-09-10 21:19:31 +0900

    utils/Constants.js作成

--- resources/js/utils/Constants.js
+++ resources/js/utils/Constants.js
@@ -1,17 +1,18 @@
 export default {
-    state:{
-        accountId: '',
-        accountName: '',
-        role: '',
-    },
-    mutations: {
-        updateUser(state, payload){
-            state.accountId = payload.accountId
-            state.accountName = payload.accountName
-            state.role = payload.role
+    ROLE:{
+        // 権限：10:確認用、20:広告主、30:代理店、40:媒体社、50:管理者
+        ADMIN: '50',
+        MEDIA: '40',
+        AGENCY: '30',
+        ADVERTISER: '20',
+        VIEWONLY: '10',
+        NAMES:{
+            '50': 'Admin',
+            '40': '媒体者',
+            '30': '代理店',
+            '20': '広告主',
+            '10': '確認用'
         }
+
     }
-
-
-
 }
