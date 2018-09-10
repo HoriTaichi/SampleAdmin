db3feddc12444d24a60c7ff7605bfcf292e31d12
Hori Taichi <thori0802@gmail.com>
2018-09-10 22:47:30 +0900

    RoleFilter作成

--- resources/js/components/filters/RoleFilter.vue
+++ resources/js/components/filters/RoleFilter.vue
@@ -1,57 +1,19 @@
-<template lang="pug">
-    span
-        el-button(@click="showFilter") {{title}} : {{buttonLabel}}
-        el-dialog(:title="title", :visible.sync="dialogVisible", @close="cancelFilter")
-            el-form()
-                template(v-for="(item) of labelList")
-                    el-form-item(size="mini")
-                        el-radio(v-model="tmpValue", :label="item") {{item.label}}
-            span.dialog-footer(slot="footer")
-                el-button(@click="cancelFilter") キャンセル
-                el-button(@click="submitFilter") 適用
-</template>
 <script>
+    import ChoiceFilter from '@/components/filters/ChoiceFilter'
     export default {
-        abstract: true, // 抽象クラス
-        props:{
-            value: String
-        },
-        data() {
+        extends : ChoiceFilter,
+        abstract : false,
+        data(){
             return {
-                dialogVisible: false,
-                tmpValue :'',
-                title: '',
-                labelList: []
-            };
-        },
-        computed:{
-            buttonLabel: function(){
-                let label = ''
-                for(let item of this.labelList){
-                    if(item.value === this.value){
-                        label = item.label
-                    }
-                }
-                return label
-            }
-        },
-        methods: {
-            showFilter: function(){
-                for(let item of this.labelList){
-                    if(item.value === this,value){
-                        this,tmpValue = item
-                    }
-                }
-                this.dialogVisible = true
-            },
-            cancelFilter: function(){
-                this.dialogVisible = false
-                this.tmpValue = this.value
-            },
-            submitFilter: function(){
-                this.dialogVisible = false
-                this.$emit('input', this.tmpValue.value)
-                this.$emit('changeValue')
+                title : 'ロール',
+                labelList:[
+                    {value: '', label: 'すべて'},
+                    {value: '50', label: 'Admin'},
+                    {value: '40', label: '媒体者'},
+                    {value: '30', label: '代理店'},
+                    {value: '20', label: '広告主'},
+                    {value: '10', label: '確認用'},
+                ]
             }
         }
     }
