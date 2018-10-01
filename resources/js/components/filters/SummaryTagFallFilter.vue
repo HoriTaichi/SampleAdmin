ef3ce2206cac401e8e0f9d3a3d1a938aaa709f9f
Hori Taichi <thori0802@gmail.com>
2018-10-02 08:36:34 +0900

    合計欄のフィルター実装

---
+++ resources/js/components/filters/SummaryTagFallFilter.vue
@@ -0,0 +1,18 @@
+<script>
+    import ChoiceFilter from '@/components/filters/ChoiceFilter'
+    export default {
+        extends : ChoiceFilter,
+        abstract : false,
+        data(){
+            return {
+                title : '合計欄',
+                labelList:[
+                    {value: 'tagFallSum', label: '落下imp数'},
+                    {value: 'tagFallCount', label: '停止数'},
+                ]
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
